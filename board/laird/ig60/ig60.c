05f87750d2badfc489df1b8840cf23da3bf9a7fa
Vishal Verma <vishal.verma@lairdtech.com>
2018-07-25 16:21:20 -0400

    laird: ig60: Add a new board file for IG60
    Add board file for IG60 and support led initialization on uboot start.
    
    bug 13185

---
+++ board/laird/ig60/ig60.c
@@ -0,0 +1,287 @@
+/*
+ * Copyright (C) 2018 Laird
+ * Ben Whitten <ben.whitten@lairdtech.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <asm/io.h>
+#include <asm/arch/sama5_sfr.h>
+#include <asm/arch/sama5d3_smc.h>
+#include <asm/arch/at91_common.h>
+#include <asm/arch/at91_rstc.h>
+#include <asm/arch/gpio.h>
+#include <asm/arch/clk.h>
+#include <debug_uart.h>
+#include <spl.h>
+#include <asm/arch/atmel_mpddrc.h>
+#include <asm/arch/at91_wdt.h>
+#include <asm/arch/atmel_usba_udc.h>
+#include <linux/ctype.h>
+#include <uboot_aes.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#define FS_MAX_KEY_SIZE	64
+#define FS_KEY_WINDOW	0x31e000
+
+struct key_prop {
+	const void *cipher_key;
+	int cipher_key_len;
+	const void *cmac_key;
+	int cmac_key_len;
+	const void *iv;
+};
+
+void ig60_nand_hw_init(void)
+{
+	struct at91_smc *smc = (struct at91_smc *)ATMEL_BASE_SMC;
+
+	at91_periph_clk_enable(ATMEL_ID_SMC);
+
+	/* Configure SMC CS3 for NAND/SmartMedia */
+	writel(AT91_SMC_SETUP_NWE(2) | AT91_SMC_SETUP_NCS_WR(1) |
+	       AT91_SMC_SETUP_NRD(2) | AT91_SMC_SETUP_NCS_RD(1),
+	       &smc->cs[3].setup);
+	writel(AT91_SMC_PULSE_NWE(2) | AT91_SMC_PULSE_NCS_WR(4) |
+	       AT91_SMC_PULSE_NRD(2) | AT91_SMC_PULSE_NCS_RD(4),
+	       &smc->cs[3].pulse);
+	writel(AT91_SMC_CYCLE_NWE(4) | AT91_SMC_CYCLE_NRD(4),
+	       &smc->cs[3].cycle);
+	writel(AT91_SMC_TIMINGS_TCLR(2) | AT91_SMC_TIMINGS_TADL(11) |
+	       AT91_SMC_TIMINGS_TAR(2)  | AT91_SMC_TIMINGS_TRR(3)   |
+	       AT91_SMC_TIMINGS_TWB(5)  | AT91_SMC_TIMINGS_RBNSEL(3)|
+	       AT91_SMC_TIMINGS_NFSEL(1), &smc->cs[3].timings);
+	writel(AT91_SMC_MODE_RM_NRD | AT91_SMC_MODE_WM_NWE |
+	       AT91_SMC_MODE_EXNW_DISABLE |
+	       AT91_SMC_MODE_DBW_8 |
+	       AT91_SMC_MODE_TDF_CYCLE(9), /* 65ns, 7.5757ns clock = 8.58 */
+	       &smc->cs[3].mode);
+
+	/* Disable Flash Write Protect Line */
+	at91_set_pio_output(AT91_PIO_PORTE, 14, 1);
+}
+
+static void ig60_usb_hw_init(void)
+{
+    at91_udp_hw_init();
+
+    usba_udc_probe(&pdata);
+}
+
+void board_fit_image_post_process(void **p_image, size_t *p_size)
+{
+	u8 *image = (u8 *)*p_image;
+	int enc_node, aes_blocks;
+	struct key_prop prop;
+	u8 key_exp[AES_EXPAND_KEY_LENGTH];
+	u8 padding;
+
+	enc_node = fdt_subnode_offset(gd->fdt_blob, 0, "encryption");
+	if (enc_node < 0) {
+		debug("No encryption node found\n");
+		return;
+	}
+
+	prop.cipher_key = fdt_getprop(gd->fdt_blob, enc_node, "aes,cipher-key", &prop.cipher_key_len);
+	prop.cmac_key = fdt_getprop(gd->fdt_blob, enc_node, "aes,cmac-key", &prop.cmac_key_len);
+	prop.iv = fdt_getprop(gd->fdt_blob, enc_node, "aes,iv", NULL);
+
+	aes_expand_key((u8 *)prop.cipher_key, key_exp);
+
+	puts("   Decrypting ... ");
+	/* got to be 128bit */
+	aes_blocks = DIV_ROUND_UP(*p_size, prop.cipher_key_len);
+	aes_cbc_decrypt_blocks(key_exp, (u8 *)prop.iv, image, image, aes_blocks);
+	puts("OK\n");
+
+	/* Reduce PKCS7 padded length */
+	padding = *(image + (*p_size - 1));
+	*p_size = *p_size - padding;
+}
+
+void ig60_led_init(void)
+{
+	at91_set_pio_output(AT91_PIO_PORTA, 1, 1);
+	at91_set_pio_output(AT91_PIO_PORTA, 3, 1);
+	at91_set_pio_output(AT91_PIO_PORTA, 6, 1);
+	at91_set_pio_output(AT91_PIO_PORTA, 13, 1);
+	at91_set_pio_output(AT91_PIO_PORTA, 15, 1);
+	at91_set_pio_output(AT91_PIO_PORTA, 16, 1);
+	at91_set_pio_output(AT91_PIO_PORTA, 17, 0); //ENABLE POWER_GREEN_LED
+	at91_set_pio_output(AT91_PIO_PORTA, 18, 1);
+	at91_set_pio_output(AT91_PIO_PORTA, 25, 1);
+	at91_set_pio_output(AT91_PIO_PORTA, 27, 1);
+	at91_set_pio_output(AT91_PIO_PORTA, 29, 0); //ENABLE POWER_RED_LED
+}
+void ig60_fs_key_inject(void)
+{
+	u8	*key = (u8 *)FS_KEY_WINDOW;
+	const void *fs_key;
+	int fs_key_len;
+	int enc_node;
+
+	enc_node = fdt_subnode_offset(gd->fdt_blob, 0, "encryption");
+	if (enc_node < 0) {
+		debug("No encryption node found\n");
+		return;
+	}
+
+	fs_key = fdt_getprop(gd->fdt_blob, enc_node, "laird,fs-key", &fs_key_len);
+	if (!fs_key) {
+		debug("No fs-key property found\n");
+		return;
+	}
+
+	if (fs_key_len != FS_MAX_KEY_SIZE) {
+		debug("Key must be max size\n");
+		return;
+	}
+
+	memcpy(key, fs_key, fs_key_len);
+
+	return;
+}
+
+void board_debug_uart_init(void)
+{
+	at91_seriald_hw_init();
+}
+
+int board_late_init(void)
+{
+	const char *LAIRD_NAME = "lrd_name";
+	char name[32], *p;
+
+	strcpy(name, get_cpu_name());
+	for (p = name; *p != '\0'; *p = tolower(*p), p++)
+		;
+	strcat(name, "-ig60");
+	env_set(LAIRD_NAME, name);
+
+    return 0;
+}
+
+int board_early_init_f(void)
+{
+	debug_uart_init();
+
+    return 0;
+}
+
+int board_init(void)
+{
+	/* adress of boot parameters */
+	gd->bd->bi_boot_params = CONFIG_SYS_SDRAM_BASE + 0x100;
+
+	ig60_nand_hw_init();
+	ig60_usb_hw_init();
+	ig60_led_init();
+	ig60_fs_key_inject();
+
+
+	return 0;
+}
+
+int dram_init(void)
+{
+	gd->ram_size = get_ram_size((void *)CONFIG_SYS_SDRAM_BASE,
+				    CONFIG_SYS_SDRAM_SIZE);
+
+	return 0;
+}
+
+
+/* SPL */
+#ifdef CONFIG_SPL_BUILD
+void spl_board_init(void)
+{
+	ig60_nand_hw_init();
+}
+
+#ifdef CONFIG_SPL_LOAD_FIT
+int board_fit_config_name_match(const char *name)
+{
+	/* Just empty function now - can't decide what to choose */
+	debug("%s: %s\n", __func__, name);
+
+	return 0;
+}
+#endif
+
+static void ddr2_conf(struct atmel_mpddrc_config *ddr2)
+{
+	ddr2->md = (ATMEL_MPDDRC_MD_DBW_32_BITS | ATMEL_MPDDRC_MD_LPDDR_SDRAM);
+
+	ddr2->cr = (ATMEL_MPDDRC_CR_NC_COL_10 | //or 11 from DS
+		    ATMEL_MPDDRC_CR_NR_ROW_13 |
+		    ATMEL_MPDDRC_CR_CAS_DDR_CAS3 |
+		    ATMEL_MPDDRC_CR_ENRDM_ON |
+		    ATMEL_MPDDRC_CR_NDQS_DISABLED |
+		    ATMEL_MPDDRC_CR_DECOD_INTERLEAVED |
+		    ATMEL_MPDDRC_CR_UNAL_SUPPORTED);
+	/*
+	 * As the DDR2-SDRAm device requires a refresh time is 7.8125us
+	 * when DDR run at 133MHz, so it needs (7.8125us * 133MHz / 10^9) clocks
+	 */
+    /* TODO DDR running at 132MHz, 7.5757ns <--- NOT 133Mhz */
+
+	ddr2->rtr = 0x411;
+
+	ddr2->tpr0 = (6 << ATMEL_MPDDRC_TPR0_TRAS_OFFSET |
+		      2 << ATMEL_MPDDRC_TPR0_TRCD_OFFSET |
+		      2 << ATMEL_MPDDRC_TPR0_TWR_OFFSET |
+		      8 << ATMEL_MPDDRC_TPR0_TRC_OFFSET |
+		      2 << ATMEL_MPDDRC_TPR0_TRP_OFFSET |
+		      2 << ATMEL_MPDDRC_TPR0_TRRD_OFFSET |
+		      2 << ATMEL_MPDDRC_TPR0_TWTR_OFFSET |
+		      2 << ATMEL_MPDDRC_TPR0_TMRD_OFFSET);
+
+	ddr2->tpr1 = (2 << ATMEL_MPDDRC_TPR1_TXP_OFFSET |
+		      200 << ATMEL_MPDDRC_TPR1_TXSRD_OFFSET |
+		      19 << ATMEL_MPDDRC_TPR1_TXSNR_OFFSET |
+		      18 << ATMEL_MPDDRC_TPR1_TRFC_OFFSET);
+
+	ddr2->tpr2 = (7 << ATMEL_MPDDRC_TPR2_TFAW_OFFSET |
+		      2 << ATMEL_MPDDRC_TPR2_TRTP_OFFSET |
+		      3 << ATMEL_MPDDRC_TPR2_TRPA_OFFSET |
+		      7 << ATMEL_MPDDRC_TPR2_TXARDS_OFFSET |
+		      2 << ATMEL_MPDDRC_TPR2_TXARD_OFFSET);
+}
+
+void mem_init(void)
+{
+	struct atmel_sfr *sfr = (struct atmel_sfr *)ATMEL_BASE_SFR;
+	struct atmel_mpddrc_config ddr2;
+
+	ddr2_conf(&ddr2);
+
+	writel(ATMEL_SFR_DDRCFG_FDQIEN | ATMEL_SFR_DDRCFG_FDQSIEN,
+	       &sfr->ddrcfg);
+
+	/* Enable MPDDR clock */
+	at91_periph_clk_enable(ATMEL_ID_MPDDRC);
+	at91_system_clk_enable(AT91_PMC_DDR);
+
+	/* DDRAM2 Controller initialize */
+	ddr2_init(ATMEL_BASE_MPDDRC, ATMEL_BASE_DDRCS, &ddr2);
+}
+
+void at91_pmc_init(void)
+{
+	u32 tmp;
+
+	tmp = AT91_PMC_PLLAR_29 |
+	      AT91_PMC_PLLXR_PLLCOUNT(0x3f) |
+	      AT91_PMC_PLLXR_MUL(43) |
+	      AT91_PMC_PLLXR_DIV(1);
+	at91_plla_init(tmp);
+
+	at91_pllicpr_init(AT91_PMC_IPLL_PLLA(0x3));
+
+	tmp = AT91_PMC_MCKR_MDIV_4 |
+	      AT91_PMC_MCKR_CSS_PLLA;
+	at91_mck_init(tmp);
+}
+#endif
---
+++ include/configs/ig60.h
@@ -0,0 +1,127 @@
+/*
+ * Configuation settings for the SOM60 Module.
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define CONFIG_SYS_TEXT_BASE		0x26f00000
+
+/* ARM asynchronous clock */
+#define CONFIG_SYS_AT91_SLOW_CLOCK      32768
+#define CONFIG_SYS_AT91_MAIN_CLOCK      12000000 /* from 12 MHz crystal */
+
+#define CONFIG_ARCH_CPU_INIT
+
+#define CONFIG_CMDLINE_TAG	/* enable passing of ATAGs */
+
+#ifndef CONFIG_SPL_BUILD
+#define CONFIG_SKIP_LOWLEVEL_INIT
+#endif
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"bootside=a\0"						\
+	"_setbootvol=if test ${bootside} = a;then "	\
+			"setenv bootvol 1;"			\
+		"else "							\
+			"setenv bootvol 4;"			\
+		"fi\0"							\
+	"_formatubi=nand erase.part ubi;"							\
+		"ubi part ubi;"											\
+		"for part in a b; do "									\
+			"ubi create kernel_${part} 800000 static;"			\
+			"ubi create rootfs_${part} 5200000 static;"			\
+			"ubi create rootfs_data_${part} 1E00000 dynamic;"	\
+		"done\0"
+
+#define CONFIG_ENV_VARS_UBOOT_CONFIG
+
+/*
+ * BOOTP options
+ */
+#define CONFIG_BOOTP_BOOTFILESIZE
+#define CONFIG_BOOTP_BOOTPATH
+#define CONFIG_BOOTP_GATEWAY
+#define CONFIG_BOOTP_HOSTNAME
+
+/*
+ * Command line configuration.
+ */
+
+/* u-boot env in nand flash */
+#define CONFIG_ENV_OFFSET		0xA0000
+#define CONFIG_ENV_OFFSET_REDUND	0xC0000
+#define CONFIG_ENV_SIZE			0x20000
+
+
+#define CONFIG_SYS_LONGHELP
+#define CONFIG_CMDLINE_EDITING
+#define CONFIG_AUTO_COMPLETE
+
+/* Size of malloc() pool */
+#define CONFIG_SYS_MALLOC_LEN		(8 * 1024 * 1024)
+
+/* SDRAM */
+#define CONFIG_NR_DRAM_BANKS		1
+#define CONFIG_SYS_SDRAM_BASE           0x20000000
+#define CONFIG_SYS_SDRAM_SIZE           0x10000000
+
+#ifdef CONFIG_SPL_BUILD
+#define CONFIG_SYS_INIT_SP_ADDR		0x318000
+#else
+#define CONFIG_SYS_INIT_SP_ADDR \
+	(CONFIG_SYS_SDRAM_BASE + 16 * 1024 - GENERATED_GBL_DATA_SIZE)
+#endif
+
+/* NAND flash */
+#define CONFIG_NAND_ATMEL
+#define CONFIG_SYS_MAX_NAND_DEVICE	1
+#define CONFIG_SYS_NAND_BASE		0x60000000
+/* our ALE is AD21 */
+#define CONFIG_SYS_NAND_MASK_ALE	(1 << 21)
+/* our CLE is AD22 */
+#define CONFIG_SYS_NAND_MASK_CLE	(1 << 22)
+#define CONFIG_SYS_NAND_ONFI_DETECTION
+
+#define CONFIG_MTD_DEVICE
+#define CONFIG_MTD_PARTITIONS
+
+/* PMECC & PMERRLOC */
+#define CONFIG_ATMEL_NAND_HWECC
+#define CONFIG_ATMEL_NAND_HW_PMECC
+#define CONFIG_PMECC_CAP		    8
+#define CONFIG_PMECC_SECTOR_SIZE	512
+
+
+#define CONFIG_SYS_LOAD_ADDR			0x21000000 /* load address */
+
+/* SPL */
+#define CONFIG_SPL_FRAMEWORK
+#define CONFIG_SPL_TEXT_BASE		0x300000
+#define CONFIG_SPL_MAX_SIZE			0x10000
+#define CONFIG_SPL_BSS_START_ADDR	0x20000000
+#define CONFIG_SPL_BSS_MAX_SIZE		0x80000
+#define CONFIG_SYS_SPL_MALLOC_START	0x20080000
+#define CONFIG_SYS_SPL_MALLOC_SIZE	0x80000
+
+#define CONFIG_SYS_MONITOR_LEN		(512 << 10)
+
+#ifdef CONFIG_SD_BOOT
+#define CONFIG_SYS_MMCSD_FS_BOOT_PARTITION	1
+#define CONFIG_SPL_FS_LOAD_PAYLOAD_NAME	"u-boot.itb"
+
+#elif CONFIG_NAND_BOOT
+#define CONFIG_SPL_NAND_DRIVERS
+#define CONFIG_SPL_NAND_BASE
+#endif
+#define CONFIG_SYS_NAND_5_ADDR_CYCLE
+#define CONFIG_SYS_NAND_PAGE_SIZE	0x800
+#define CONFIG_SYS_NAND_PAGE_COUNT	64
+#define CONFIG_SYS_NAND_OOBSIZE		64
+#define CONFIG_SYS_NAND_BLOCK_SIZE	0x20000
+#define CONFIG_SYS_NAND_BAD_BLOCK_POS	0x0
+#define CONFIG_SPL_GENERATE_ATMEL_PMECC_HEADER
+
+#endif
