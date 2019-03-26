90c00cec1b80e5652ff34004e8caac1da96ccf90
Don Ferencz <Donald.Ferencz@lairdtech.com>
2019-03-26 18:00:35 +0000

    Prevent prompt in production IG bootloader
    Remove FDT command, with fixes to U-Boot for comilation errors when CMD_FDT is unset
    
    Bug 14338

--- common/bootm.c
+++ common/bootm.c
@@ -244,7 +244,9 @@ int bootm_find_images(int flag, int argc, char * const argv[])
 		puts("Could not find a valid device tree\n");
 		return 1;
 	}
+#if CONFIG_CMD_FDT
 	set_working_fdt_addr((ulong)images.ft_addr);
+#endif
 #endif
 
 #if IMAGE_ENABLE_FIT
