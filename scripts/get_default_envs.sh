a2ade10aaade4b9ed16f0b05b9564ce76a7a387b
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2023-02-13 08:21:53 -0500

    uboot: Revert retrieving default environment script fix
    We will use upstream fix
    
    Bug 23656

--- scripts/get_default_envs.sh
+++ scripts/get_default_envs.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 # SPDX-License-Identifier: GPL-2.0+
 #
+# Copyright (C) 2016, Lukasz Majewski <l.majewski@majess.pl>
+#
+
 # This file extracts default envs from built u-boot
 # usage: get_default_envs.sh [build dir] > u-boot-env-default.txt
 set -ue
 
 : "${OBJCOPY:=${CROSS_COMPILE:-}objcopy}"
-: "${OBJDUMP:=${CROSS_COMPILE:-}objdump}"
+
+ENV_OBJ_FILE="common.o"
+ENV_OBJ_FILE_COPY="copy_${ENV_OBJ_FILE}"
 
 echoerr() { echo "$@" 1>&2; }
 
@@ -14,32 +19,25 @@ if [ "$#" -eq 1 ]; then
     path=${1}
 else
     path=$(readlink -f $0)
+    path=${path%/scripts*}
 fi
 
-ENV_OBJ_FILE="${path}/u-boot"
-ENV_OBJ_FILE_RODATA="${path}/rodata_u_boot"
-ENV_OBJ_FILE_ENV="${path}/defaut_env_u_boot"
+env_obj_file_path=$(find ${path} -path "*/env/*" -not -path "*/spl/*" \
+			 -not -path "*/tools/*" -name "${ENV_OBJ_FILE}")
+[ -z "${env_obj_file_path}" ] && \
+    { echoerr "File '${ENV_OBJ_FILE}' not found!"; exit 1; }
 
-if [ ! -f ${ENV_OBJ_FILE} ]; then
-    echoerr "File '${ENV_OBJ_FILE}' not found!"
-    exit 1
-fi
+cp ${env_obj_file_path} ${ENV_OBJ_FILE_COPY}
 
-${OBJCOPY} --dump-section .rodata=${ENV_OBJ_FILE_RODATA} ${ENV_OBJ_FILE}
-set -- $(${OBJDUMP} -x ${ENV_OBJ_FILE} |grep \.rodata$)
-start=${1}
+# NOTE: objcopy saves its output to file passed in
+# (copy_${ENV_OBJ_FILE} in this case)
 
-set -- $(${OBJDUMP} -x ${ENV_OBJ_FILE} |grep default_environment$)
-stop=${1}
-length=$((0x${5}))
+${OBJCOPY} --dump-section .rodata.default_environment=${ENV_OBJ_FILE_COPY} \
+	${env_obj_file_path}
 
-count=$((0x${stop} - 0x${start}))
+# Replace default '\0' with '\n' , remove blank lines and sort entries
+tr '\0' '\n' < ${ENV_OBJ_FILE_COPY} | sed -e '/^\s*$/d' | sort --field-separator== -k1,1 --stable
 
-dd skip=${count} count=${length} if=${ENV_OBJ_FILE_RODATA} of=${ENV_OBJ_FILE_ENV} bs=1
-
-# Replace default '\0' with '\n', remove blank lines and sort entries
-tr '\0' '\n' < ${ENV_OBJ_FILE_ENV} | sed -e '/^\s*$/d' | sort --field-separator== -k1,1 --stable
-
-rm -f ${ENV_OBJ_FILE_RODATA} ${ENV_OBJ_FILE_ENV}
+rm ${ENV_OBJ_FILE_COPY}
 
 exit 0
