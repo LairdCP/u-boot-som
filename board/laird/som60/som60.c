3849e1166cda54e166f25d0964d1699bfa4dc3e7
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-04-27 23:51:32 -0400

    Removed 1.3 sec delay during slow clock init
    The actual problem of slow clock init  hang was that timer used
    for delays was not created because of missing property in device
    tree, that  was fixed by commit
    
    ARM: dts: at91: sama5d3: add u-boot properties to sama5d3 pit timer
    
    PROD-9902

--- board/laird/som60/som60.c
+++ board/laird/som60/som60.c
@@ -779,17 +779,11 @@ static void at91sama5d3_slowclock_init(void)
 	tmp = readl(reg);
 	tmp &= ~AT91SAM9G45_SCKCR_RCEN;
 	writel(tmp, reg);
-
-	/* Delay makes boot much more stable
-	 * This delay present on boostrap for the case of the oscillator
-	 * not in bypass mode, which is all Microchip demo boards
-	 * My guess Microchip misattributed the reson for delay need
-	 */
-	udelay(1300000);
 }
 
 void spl_board_init(void)
 {
+	/* Run after spl_early_init because using timer delay */
 	at91sama5d3_slowclock_init();
 
 	at91_disable_smd_clock();
